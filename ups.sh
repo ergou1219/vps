ec2b6ffe77079164511be03668bb0be73c2d7e94
ergou <58904481+ergou1219@users.noreply.github.com>
2023-02-26 15:45:17 +0800

    Create ups.sh

---
+++ ups.sh
@@ -0,0 +1,10 @@
+yum -y install ntpdate
+timedatectl set-timezone Asia/Shanghai
+ntpdate ntp1.aliyun.com
+systemctl disable firewalld
+systemctl stop firewalld
+yum install libsodium -y
+yum install epel-release -y
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
+chmod +x tcp.sh
+./tcp.sh
